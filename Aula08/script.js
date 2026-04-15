88775ad7ec91f47417b08cc279c2ea12d3ec398f
GabrielCNavarro00 <gabriel.card.navarro@gmail.com>
2026-04-15 15:11:25 -0300

    vgvhhhhvhvhjhvjkhvljhv

--- Aula08/script.js
+++ Aula08/script.js
@@ -1,9 +1,10 @@
+
 let retangulo_1 = {
     h: 50,
     w: 50,
     x: 100,
     y: 40,
-    color: "red" 
+    cor: "red" 
 };
 
 let retangulo_2 = {
@@ -11,19 +12,47 @@ let retangulo_2 = {
     y: 100,
     w: 50,
     h: 50,
-    color: "blue"
+    cor: "blue"
 };
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 function desenhaRetangulo(ret){
-    ctx.beginPath();
-    ctx.lineWidt = 5;
-    ctx.fillStyle = ret.color;
+    ctx.fillStyle = ret.cor;
     ctx.fillRect(ret.x, ret.y, ret.w, ret.h);
-    ctx.closePath()
 }
 
-desenhaRetangulo(retangulo_1)
-desenhaRetangulo(retangulo_2)
+let mov = 2;
+
+function desenhar(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (retangulo_1.x >= canvas.width - retangulo_1.w) {
+        mov = -mov;
+    }
+
+    if (retangulo_1.x <= 0) {
+        mov = -mov;
+    }
+
+    retangulo_1.x += mov;
+
+    desenhaRetangulo(retangulo_1);
+    desenhaRetangulo(retangulo_2);
+
+    requestAnimationFrame(desenhar);
+}
+
+document.addEventListener("keydown", function(evento){
+    var tecla = evento.key;
+    var vel = 5;
+
+    if (tecla == "ArrowUp")    retangulo_2.y -= vel;
+    if (tecla == "ArrowDown")  retangulo_2.y += vel;
+    if (tecla == "ArrowLeft")  retangulo_2.x -= vel;
+    if (tecla == "ArrowRight") retangulo_2.x += vel;
+});
+
+desenhar();
+
