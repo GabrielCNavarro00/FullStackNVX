908bd1e9bb89dc01b833879a2047adbbda3f1fbf
GabrielCNavarro00 <gabriel.card.navarro@gmail.com>
2026-04-15 15:18:53 -0300

    lksmdçlbmskd

--- Aula08/script.js
+++ Aula08/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 let retangulo_1 = {
     h: 50,
     w: 50,
@@ -57,34 +55,3 @@ document.addEventListener("keydown", function(evento){
 
 desenhar();
 
-=======
-let retangulo_1 = {
-    h: 50,
-    w: 50,
-    x: 100,
-    y: 40,
-    color: "red" 
-};
-
-let retangulo_2 = {
-    x: 100,
-    y: 100,
-    w: 50,
-    h: 50,
-    color: "blue"
-};
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-function desenhaRetangulo(ret){
-    ctx.beginPath();
-    ctx.lineWidt = 5;
-    ctx.fillStyle = ret.color;
-    ctx.fillRect(ret.x, ret.y, ret.w, ret.h);
-    ctx.closePath()
-}
-
-desenhaRetangulo(retangulo_1)
-desenhaRetangulo(retangulo_2)
->>>>>>> 9c972e3015a5212d48f9ce57c2671211f5be6451
